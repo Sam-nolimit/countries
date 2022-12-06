55dc27e17447d9e7b3d33a44a932ab7ff1a8420f
Temitope Adejolu <tadejolu@gmail.com>
2022-12-06 23:34:27 +0100

    Release(world-countries):google earth implementation [finishes#0611123]

--- src/pages/CountryDetail.tsx
+++ src/pages/CountryDetail.tsx
@@ -2,13 +2,15 @@ import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import Navbar from "../components/Navbar";
 import { themeState } from "../lib/recoil/atoms";
+import "./CountryDetails.css";
 
 const CountryDetail = () => {
   return (
     <>
       <Navbar />
       <main className="flex min-h-screen flex-col justify-start bg-[#eeeeee] py-8 px-6 dark:bg-[#202c37] lg:px-12 xl:px-16 2xl:px-32">
-        hello
+        <button id="button" type="button"> click</button>
+        <a href="https://earth.google.com/web/@0,-5.5833,0a,22251752.77375655d,35y,0h,0t,0r"> country location</a>
       </main>
     </>
   );
